4e565755941e55835164e1e112589e893b07bd18
Abisha-SM <abisha1720997@gmail.com>
2024-05-20 16:32:56 +0530

    -Added moving Obstacles

---
+++ Assets/Script/Obstacles.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Obstacles : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float distance = 2f;
+        float speed = 1f;
+
+        float yPos = transform.position.y;
+        float zPos = transform.position.z;
+        float xPos = Mathf.Sin(Time.time * speed) * distance;
+
+        transform.position = new Vector3(xPos, yPos, zPos);
+    }
+}
