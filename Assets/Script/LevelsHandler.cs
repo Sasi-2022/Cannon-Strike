a42c47c34b531523ce0918e29c69e00b1ddfc820
Abisha-SM <abisha1720997@gmail.com>
2024-05-22 22:49:36 +0530

    -Added level item start button functionality

--- Assets/Script/LevelsHandler.cs
+++ Assets/Script/LevelsHandler.cs
@@ -2,9 +2,23 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
 
 public class LevelsHandler : MonoBehaviour
 {
     public int id;
     public TextMeshProUGUI leveltext;
+    public Button startbtn;
+
+    public void Start()
+    {
+        startbtn.onClick.AddListener(OnclickLevel);
+    }
+
+    public void OnclickLevel()
+    {
+        SceneManager.LoadScene(2);
+    }
 }
