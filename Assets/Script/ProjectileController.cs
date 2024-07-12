6d30c9750456e77d10f0c23d9b6360def0d9d126
Sasi-2022 <sasikalaumapathy0609@gmail.com>
2024-07-12 12:37:50 +0530

    -Updated Bugs

--- Assets/Script/ProjectileController.cs
+++ Assets/Script/ProjectileController.cs
@@ -33,8 +33,251 @@ public class ProjectileController : MonoBehaviour
             Rigidbody2D rb = GetComponent<Rigidbody2D>();
             if (rb != null)
             {
-                Vector2 direction = (collision.transform.position - transform.position).normalized;
-                rb.AddForce(-direction * 45f, ForceMode2D.Impulse);
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 1)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 80f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 2)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 80f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 3)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 15f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 4)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 25f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 5)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 15f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 6)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 7)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 8)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 9)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 10)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 18f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 11)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 12)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 15f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 13)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 15f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 14)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 14f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 15)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 16)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 17)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 18)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 16f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 19)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 20)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 21)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 22)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 16f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 23)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 24)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 25)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 26)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 16f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 27)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 28)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 30)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 31)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 32)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 33)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 34)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 35)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 36)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 37)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 38)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 39)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 40)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 41)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 16f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 42)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 16f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 43)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 16f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 44)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 16f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 45)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 46)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 47)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 48)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 49)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
+                if (GameplayController.instance.PlayerDataSO.player.PlayerCurrentLevel == 50)
+                {
+                    Vector2 direction = (collision.transform.position - transform.position).normalized;
+                    rb.AddForce(-direction * 20f, ForceMode2D.Impulse);
+                }
             }
         }
        else if (collision.collider.CompareTag("Collector"))
