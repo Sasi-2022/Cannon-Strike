2dd1253ce1f3b2ff1dec119967d6e61a96831f31
Abisha-SM <abisha1720997@gmail.com>
2024-11-12 16:49:02 +0530

    -Added scroll

--- Assets/Script/Levels.cs
+++ Assets/Script/Levels.cs
@@ -97,4 +97,291 @@ public class Levels : MonoBehaviour
         SceneManager.LoadScene(3);
         //level4.Invoke();
     }
+    public void btn_level10()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level10";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level11()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level11";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level12()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level12";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level13()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level13";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level14()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level14";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level15()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level15";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level16()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level16";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level17()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level17";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level18()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level18";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level19()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level19";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level20()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level20";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level21()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level21";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level22()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level22";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level23()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level23";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level24()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level24";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level25()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level25";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level26()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level26";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level27()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level27";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level28()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level28";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level29()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level29";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level30()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level30";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level31()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level31";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level32()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level32";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level33()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level33";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level34()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level34";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level35()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level35";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level36()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level36";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level37()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level37";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level38()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level38";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level39()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level39";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level40()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level40";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level41()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level41";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level42()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level42";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level43()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level43";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level44()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level44";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level45()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level45";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level46()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level46";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level47()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level47";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level48()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level48";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level49()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level49";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
+    public void btn_level50()
+    {
+        Time.timeScale = 1f;
+        pickedlevels = "Level50";
+        SceneManager.LoadScene(3);
+        //level4.Invoke();
+    }
 }
